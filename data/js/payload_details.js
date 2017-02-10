5df24bdb3f215e5aed6748e74c5c562d8fed375d
klferguson <klferguson@users.noreply.github.com>
2017-02-10 09:12:25 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41700,
-	"created_at": "Fri Feb 10 13:12:24 +0000 2017",
+	"tweets": 41701,
+	"created_at": "Fri Feb 10 14:12:24 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_02.js
+++ data/js/tweets/2017_02.js
@@ -1,6 +1,422 @@
 Grailbird.data.tweets_2017_02 = 
 [
 	{
+		"created_at": "Fri Feb 10 13:41:25 +0000 2017",
+		"id": 830049030738677800,
+		"id_str": "830049030738677761",
+		"text": "RT @PostFilm: Beijing: postmodern? #theory #highered https://t.co/QuVNYMF9HJ",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "theory",
+					"indices": [
+						35,
+						42
+					]
+				},
+				{
+					"text": "highered",
+					"indices": [
+						43,
+						52
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PostFilm",
+					"name": "Dr Ian McCormick",
+					"id": 107943025,
+					"id_str": "107943025",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 830027139198099500,
+					"id_str": "830027139198099457",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+					"url": "https://t.co/QuVNYMF9HJ",
+					"display_url": "pic.twitter.com/QuVNYMF9HJ",
+					"expanded_url": "https://twitter.com/PostFilm/status/830027162086408193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 577,
+							"h": 423,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 577,
+							"h": 423,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 577,
+							"h": 423,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 830027162086408200,
+					"source_status_id_str": "830027162086408193",
+					"source_user_id": 107943025,
+					"source_user_id_str": "107943025"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 830027139198099500,
+					"id_str": "830027139198099457",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+					"url": "https://t.co/QuVNYMF9HJ",
+					"display_url": "pic.twitter.com/QuVNYMF9HJ",
+					"expanded_url": "https://twitter.com/PostFilm/status/830027162086408193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 577,
+							"h": 423,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 577,
+							"h": 423,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 577,
+							"h": 423,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 830027162086408200,
+					"source_status_id_str": "830027162086408193",
+					"source_user_id": 107943025,
+					"source_user_id_str": "107943025"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "Asst Prof English, Queens College/CUNY • DH + media studies, 1980s, Acker, horror • https://t.co/PpwQ9i80xQ • https://t.co/nMrHfpkwlb",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PpwQ9i80xQ",
+							"expanded_url": "http://palgrave.com/us/book/9781137586384",
+							"display_url": "palgrave.com/us/book/978113…",
+							"indices": [
+								84,
+								107
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfpkwlb",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1041,
+			"friends_count": 461,
+			"listed_count": 47,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 11025,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 41699,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/826963635578736641/ywA0ooH__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/826963635578736641/ywA0ooH__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "1500FF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Feb 10 12:14:31 +0000 2017",
+			"id": 830027162086408200,
+			"id_str": "830027162086408193",
+			"text": "Beijing: postmodern? #theory #highered https://t.co/QuVNYMF9HJ",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "theory",
+						"indices": [
+							21,
+							28
+						]
+					},
+					{
+						"text": "highered",
+						"indices": [
+							29,
+							38
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 830027139198099500,
+						"id_str": "830027139198099457",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+						"url": "https://t.co/QuVNYMF9HJ",
+						"display_url": "pic.twitter.com/QuVNYMF9HJ",
+						"expanded_url": "https://twitter.com/PostFilm/status/830027162086408193/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 577,
+								"h": 423,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 577,
+								"h": 423,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 577,
+								"h": 423,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 830027139198099500,
+						"id_str": "830027139198099457",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C4TZSRHWIAEpnpz.jpg",
+						"url": "https://t.co/QuVNYMF9HJ",
+						"display_url": "pic.twitter.com/QuVNYMF9HJ",
+						"expanded_url": "https://twitter.com/PostFilm/status/830027162086408193/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 577,
+								"h": 423,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 577,
+								"h": 423,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 577,
+								"h": 423,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 107943025,
+				"id_str": "107943025",
+				"name": "Dr Ian McCormick",
+				"screen_name": "PostFilm",
+				"location": "UK",
+				"description": "Past: Prof. Northampton Uni. M.A. St Andrews, PhD Leeds. Shakespeare and Idealism. Gothic. Art of Connection: https://t.co/DT0Wv0ZQWx",
+				"url": "https://t.co/5YFNQQXbO5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5YFNQQXbO5",
+								"expanded_url": "https://leeds.academia.edu/DrIanMcCormick",
+								"display_url": "leeds.academia.edu/DrIanMcCormick",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/DT0Wv0ZQWx",
+								"expanded_url": "http://www.amazon.co.uk/dp/B00GS5TYQ2",
+								"display_url": "amazon.co.uk/dp/B00GS5TYQ2",
+								"indices": [
+									110,
+									133
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 17468,
+				"friends_count": 13615,
+				"listed_count": 514,
+				"created_at": "Sun Jan 24 07:54:53 +0000 2010",
+				"favourites_count": 9241,
+				"utc_offset": 0,
+				"time_zone": "London",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 11738,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "8B542B",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/807653087/32c8006e1d163e3629bed236d73166ea.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/807653087/32c8006e1d163e3629bed236d73166ea.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000624298450/81c2d91b4bb64aaf80a9b9e5291c6d32_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000624298450/81c2d91b4bb64aaf80a9b9e5291c6d32_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/107943025/1425646369",
+				"profile_link_color": "9D582E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EADEAA",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 15,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Feb 10 12:46:43 +0000 2017",
 		"id": 830035267264323600,
 		"id_str": "830035267264323584",
