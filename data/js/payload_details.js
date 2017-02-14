51328c387424584eec89d2c7af336161a8714e55
klferguson <klferguson@users.noreply.github.com>
2017-02-14 16:12:26 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41785,
-	"created_at": "Tue Feb 14 20:12:24 +0000 2017",
+	"tweets": 41786,
+	"created_at": "Tue Feb 14 21:12:25 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_02.js
+++ data/js/tweets/2017_02.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2017_02 = 
 [
 	{
+		"created_at": "Tue Feb 14 20:58:36 +0000 2017",
+		"id": 831608604012712000,
+		"id_str": "831608604012711937",
+		"text": "RT @denormalize: Today Trump removed all open data (9GB) from the White House https://t.co/ELRMxTgdb2 but I grabbed it all Jan 20! Will dis…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "denormalize",
+					"name": "maxwell ogden",
+					"id": 12241752,
+					"id_str": "12241752",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ELRMxTgdb2",
+					"expanded_url": "https://open.whitehouse.gov/browse",
+					"display_url": "open.whitehouse.gov/browse",
+					"indices": [
+						78,
+						101
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "Asst Prof English, Queens College/CUNY • DH + media studies, 1980s, Acker, horror • https://t.co/PpwQ9i80xQ • https://t.co/nMrHfpkwlb",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PpwQ9i80xQ",
+							"expanded_url": "http://palgrave.com/us/book/9781137586384",
+							"display_url": "palgrave.com/us/book/978113…",
+							"indices": [
+								84,
+								107
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfpkwlb",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1041,
+			"friends_count": 461,
+			"listed_count": 47,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 11052,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 41744,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/826963635578736641/ywA0ooH__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/826963635578736641/ywA0ooH__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "1500FF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Feb 14 19:12:22 +0000 2017",
+			"id": 831581871230193700,
+			"id_str": "831581871230193664",
+			"text": "Today Trump removed all open data (9GB) from the White House https://t.co/ELRMxTgdb2 but I grabbed it all Jan 20! W… https://t.co/jNJ4uzCXs8",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ELRMxTgdb2",
+						"expanded_url": "https://open.whitehouse.gov/browse",
+						"display_url": "open.whitehouse.gov/browse",
+						"indices": [
+							61,
+							84
+						]
+					},
+					{
+						"url": "https://t.co/jNJ4uzCXs8",
+						"expanded_url": "https://twitter.com/i/web/status/831581871230193664",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 12241752,
+				"id_str": "12241752",
+				"name": "maxwell ogden",
+				"screen_name": "denormalize",
+				"location": "Portland, OR",
+				"description": "Computer programmer @dat_project. Open software, open science, open data. Amateur cat photographer @catmapper. Author of JS For Cats",
+				"url": "https://t.co/GAcgXmdLV9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/GAcgXmdLV9",
+								"expanded_url": "http://maxogden.com",
+								"display_url": "maxogden.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 18390,
+				"friends_count": 4038,
+				"listed_count": 1112,
+				"created_at": "Mon Jan 14 23:11:21 +0000 2008",
+				"favourites_count": 1044,
+				"utc_offset": -32400,
+				"time_zone": "Alaska",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 18533,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "D6F7FF",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000181139101/ckYUsb2C.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000181139101/ckYUsb2C.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/706616363599532032/b5z-Hw5g_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/706616363599532032/b5z-Hw5g_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/12241752/1478218890",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "B5E8FC",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1231,
+			"favorite_count": 1535,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1231,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Feb 14 20:06:59 +0000 2017",
 		"id": 831595614693638100,
 		"id_str": "831595614693638144",
