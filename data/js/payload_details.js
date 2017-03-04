4732d7140ae6159ed1d400a0f519293f4d8698da
klferguson <klferguson@users.noreply.github.com>
2017-03-03 22:12:24 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42243,
-	"created_at": "Sat Mar 04 00:12:23 +0000 2017",
+	"tweets": 42244,
+	"created_at": "Sat Mar 04 03:12:23 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Sat Mar 04 02:16:25 +0000 2017",
+		"id": 837849177283506200,
+		"id_str": "837849177283506177",
+		"text": "RT @poolhalljames: The Woody Guthrie Center has a Kris Kristofferson exhibit and the exhibit has this in it: https://t.co/OxtI90OhWJ",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "poolhalljames",
+					"name": "Jim Zeppelin",
+					"id": 6899522,
+					"id_str": "6899522",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 837729705570664400,
+					"id_str": "837729705570664448",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+					"url": "https://t.co/OxtI90OhWJ",
+					"display_url": "pic.twitter.com/OxtI90OhWJ",
+					"expanded_url": "https://twitter.com/poolhalljames/status/837729723555934209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 837729723555934200,
+					"source_status_id_str": "837729723555934209",
+					"source_user_id": 6899522,
+					"source_user_id_str": "6899522"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 837729705570664400,
+					"id_str": "837729705570664448",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+					"url": "https://t.co/OxtI90OhWJ",
+					"display_url": "pic.twitter.com/OxtI90OhWJ",
+					"expanded_url": "https://twitter.com/poolhalljames/status/837729723555934209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 837729723555934200,
+					"source_status_id_str": "837729723555934209",
+					"source_user_id": 6899522,
+					"source_user_id_str": "6899522"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "Asst Prof English, Queens College/CUNY • DH + media studies, 1980s, Acker, horror • https://t.co/PpwQ9i80xQ • https://t.co/nMrHfpkwlb",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PpwQ9i80xQ",
+							"expanded_url": "http://palgrave.com/us/book/9781137586384",
+							"display_url": "palgrave.com/us/book/978113…",
+							"indices": [
+								84,
+								107
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfpkwlb",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1035,
+			"friends_count": 451,
+			"listed_count": 47,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 11242,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 42199,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/826963635578736641/ywA0ooH__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/826963635578736641/ywA0ooH__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "1500FF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Mar 03 18:21:45 +0000 2017",
+			"id": 837729723555934200,
+			"id_str": "837729723555934209",
+			"text": "The Woody Guthrie Center has a Kris Kristofferson exhibit and the exhibit has this in it: https://t.co/OxtI90OhWJ",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 837729705570664400,
+						"id_str": "837729705570664448",
+						"indices": [
+							90,
+							113
+						],
+						"media_url": "http://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+						"url": "https://t.co/OxtI90OhWJ",
+						"display_url": "pic.twitter.com/OxtI90OhWJ",
+						"expanded_url": "https://twitter.com/poolhalljames/status/837729723555934209/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 675,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1152,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 837729705570664400,
+						"id_str": "837729705570664448",
+						"indices": [
+							90,
+							113
+						],
+						"media_url": "http://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C6A2uo5VAAA5jl-.jpg",
+						"url": "https://t.co/OxtI90OhWJ",
+						"display_url": "pic.twitter.com/OxtI90OhWJ",
+						"expanded_url": "https://twitter.com/poolhalljames/status/837729723555934209/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 675,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1152,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 6899522,
+				"id_str": "6899522",
+				"name": "Jim Zeppelin",
+				"screen_name": "poolhalljames",
+				"location": "JP, MA",
+				"description": "Friend of @CK1Blogs. Tells jokes, writes about weird internet stuff, comics, wrestling, seltzer, other things. email: jimroks1[at]gmail",
+				"url": "https://t.co/lPggEp5v99",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lPggEp5v99",
+								"expanded_url": "http://drylandisnotamyth.tumblr.com/",
+								"display_url": "drylandisnotamyth.tumblr.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 718,
+				"friends_count": 724,
+				"listed_count": 36,
+				"created_at": "Mon Jun 18 20:56:58 +0000 2007",
+				"favourites_count": 3242,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 23346,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000024023037/2fdd2a15234398fa685cf809edd3e7be.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000024023037/2fdd2a15234398fa685cf809edd3e7be.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/569314620205060096/B08_3cYk_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/569314620205060096/B08_3cYk_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/6899522/1399239652",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 40,
+			"favorite_count": 97,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 40,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Mar 03 23:59:36 +0000 2017",
 		"id": 837814750096552000,
 		"id_str": "837814750096551936",
