b795616357cedad568ae574ae7133226e31b1728
klferguson <klferguson@users.noreply.github.com>
2017-08-16 13:12:25 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 46830,
-	"created_at": "Wed Aug 16 16:12:24 +0000 2017",
+	"tweets": 46831,
+	"created_at": "Wed Aug 16 17:12:24 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Wed Aug 16 16:34:48 +0000 2017",
+		"id": 897859197294919700,
+		"id_str": "897859197294919681",
+		"text": "RT @elmcitytree: TL/DR: white allies must link anti-KKK sentiment to white supremacy this weekend IRL to build coalitions to defeat Trump.…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "elmcitytree",
+					"name": "Jonathan W. Gray",
+					"id": 15694778,
+					"id_str": "15694778",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "Asst Prof English, Queens College/CUNY • DH + media studies, 1980s, Acker, horror • https://t.co/PpwQ9i80xQ • https://t.co/nMrHfpkwlb",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PpwQ9i80xQ",
+							"expanded_url": "http://palgrave.com/us/book/9781137586384",
+							"display_url": "palgrave.com/us/book/978113…",
+							"indices": [
+								84,
+								107
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfpkwlb",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1104,
+			"friends_count": 459,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 13467,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 46759,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/894909259384627201/209KlCWE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/894909259384627201/209KlCWE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "1500FF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Aug 16 16:22:45 +0000 2017",
+			"id": 897856165077450800,
+			"id_str": "897856165077450752",
+			"text": "TL/DR: white allies must link anti-KKK sentiment to white supremacy this weekend IRL to build coalitions to defeat Trump. Good Luck! 17/17",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 897855565950570500,
+			"in_reply_to_status_id_str": "897855565950570496",
+			"in_reply_to_user_id": 15694778,
+			"in_reply_to_user_id_str": "15694778",
+			"in_reply_to_screen_name": "elmcitytree",
+			"user": {
+				"id": 15694778,
+				"id_str": "15694778",
+				"name": "Jonathan W. Gray",
+				"screen_name": "elmcitytree",
+				"location": "Brooklyn, NYC",
+				"description": "Assoc. Prof of English @JohnJayCollege & @CUNYEnglish, Author of https://t.co/y2rwTiwIad, Afrofuturist, freelance writer, Nerd, Lefty, #LFC",
+				"url": "https://t.co/ePXvlgQ0vw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ePXvlgQ0vw",
+								"expanded_url": "http://elmcitytree.wordpress.com",
+								"display_url": "elmcitytree.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/y2rwTiwIad",
+								"expanded_url": "http://amzn.to/12nomU0",
+								"display_url": "amzn.to/12nomU0",
+								"indices": [
+									65,
+									88
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1960,
+				"friends_count": 666,
+				"listed_count": 70,
+				"created_at": "Fri Aug 01 21:25:10 +0000 2008",
+				"favourites_count": 2368,
+				"utc_offset": 16200,
+				"time_zone": "Tehran",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17852,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/672500636/ad880ae48d39309ca02ff7993b7c0847.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/672500636/ad880ae48d39309ca02ff7993b7c0847.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/812135112418721792/fBnJUBFd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/812135112418721792/fBnJUBFd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15694778/1467593130",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 8,
+			"favorited": true,
+			"retweeted": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Aug 16 15:21:21 +0000 2017",
 		"id": 897840712825417700,
 		"id_str": "897840712825417728",
