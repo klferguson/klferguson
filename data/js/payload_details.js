273c4fc74f3d4d7a41fc54802bac34c9e347807d
klferguson <klferguson@users.noreply.github.com>
2017-09-29 17:12:36 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48129,
-	"created_at": "Fri Sep 29 20:12:33 +0000 2017",
+	"tweets": 48130,
+	"created_at": "Fri Sep 29 21:12:35 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_09.js
+++ data/js/tweets/2017_09.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2017_09 = 
 [
 	{
+		"created_at": "Fri Sep 29 20:39:50 +0000 2017",
+		"id": 913865929070710800,
+		"id_str": "913865929070710786",
+		"text": "RT @carlylane: For @SyfyWire I argued in favor of why JUPITER ASCENDING needs to be your #NextCultClassic. https://t.co/rMy43pL7DY",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NextCultClassic",
+					"indices": [
+						89,
+						105
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "carlylane",
+					"name": "Carly Lane",
+					"id": 360742914,
+					"id_str": "360742914",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "SyfyWire",
+					"name": "SYFY WIRE",
+					"id": 16013461,
+					"id_str": "16013461",
+					"indices": [
+						19,
+						28
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rMy43pL7DY",
+					"expanded_url": "http://www.syfy.com/syfywire/next-cult-classic-jupiter-ascending-should-be-recognized-as-royalty",
+					"display_url": "syfy.com/syfywire/next-…",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "Asst Prof English, Queens College/CUNY • DH + media studies, 1980s, Acker, horror • https://t.co/PpwQ9i80xQ • https://t.co/nMrHfpkwlb",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PpwQ9i80xQ",
+							"expanded_url": "http://palgrave.com/us/book/9781137586384",
+							"display_url": "palgrave.com/us/book/978113…",
+							"indices": [
+								84,
+								107
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfpkwlb",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1118,
+			"friends_count": 465,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 13980,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 48046,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/912352002537013249/vKgGDCZd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/912352002537013249/vKgGDCZd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "1500FF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Sep 29 19:15:46 +0000 2017",
+			"id": 913844772758007800,
+			"id_str": "913844772758007816",
+			"text": "For @SyfyWire I argued in favor of why JUPITER ASCENDING needs to be your #NextCultClassic. https://t.co/rMy43pL7DY",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NextCultClassic",
+						"indices": [
+							74,
+							90
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SyfyWire",
+						"name": "SYFY WIRE",
+						"id": 16013461,
+						"id_str": "16013461",
+						"indices": [
+							4,
+							13
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/rMy43pL7DY",
+						"expanded_url": "http://www.syfy.com/syfywire/next-cult-classic-jupiter-ascending-should-be-recognized-as-royalty",
+						"display_url": "syfy.com/syfywire/next-…",
+						"indices": [
+							92,
+							115
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 360742914,
+				"id_str": "360742914",
+				"name": "Carly Lane",
+				"screen_name": "carlylane",
+				"location": "new york, ny.",
+				"description": "I hope no one minds if I livetweet this. Co-host @SupergirlRadio, co-founder @LegionofWomen, writes @Nerdist, @SYFYFangrrls/@SYFYWire. (Avi by @eiffelart.)",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6724,
+				"friends_count": 998,
+				"listed_count": 211,
+				"created_at": "Tue Aug 23 18:00:32 +0000 2011",
+				"favourites_count": 231473,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 65072,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FAF8D9",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/682223852978585600/nb8-_ft3.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/682223852978585600/nb8-_ft3.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/837496998517489664/WYOCyNGr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/837496998517489664/WYOCyNGr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/360742914/1506615756",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "738F80",
+				"profile_sidebar_fill_color": "22180F",
+				"profile_text_color": "A89258",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 33,
+			"favorite_count": 90,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Sep 29 19:59:29 +0000 2017",
 		"id": 913855771980517400,
 		"id_str": "913855771980517376",
