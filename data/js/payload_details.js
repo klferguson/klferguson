daa4ade19046166f0998dd941cddf05127e73357
klferguson <klferguson@users.noreply.github.com>
2017-11-28 22:12:29 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 49824,
-	"created_at": "Wed Nov 29 00:12:31 +0000 2017",
+	"tweets": 49825,
+	"created_at": "Wed Nov 29 03:12:28 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Wed Nov 29 02:12:53 +0000 2017",
+		"id": 935693012230910000,
+		"id_str": "935693012230909952",
+		"text": "RT @alexdecampi: OH MY DEAR GOD https://t.co/7oL58M5KXv",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "alexdecampi",
+					"name": "Alex de Campi",
+					"id": 15182368,
+					"id_str": "15182368",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 935215379340845000,
+					"id_str": "935215379340845056",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+					"url": "https://t.co/7oL58M5KXv",
+					"display_url": "pic.twitter.com/7oL58M5KXv",
+					"expanded_url": "https://twitter.com/alexdecampi/status/935215385971970049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 942,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 541,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 942,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 935215385971970000,
+					"source_status_id_str": "935215385971970049",
+					"source_user_id": 15182368,
+					"source_user_id_str": "15182368"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 935215379340845000,
+					"id_str": "935215379340845056",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+					"url": "https://t.co/7oL58M5KXv",
+					"display_url": "pic.twitter.com/7oL58M5KXv",
+					"expanded_url": "https://twitter.com/alexdecampi/status/935215385971970049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 942,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 541,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 942,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 935215385971970000,
+					"source_status_id_str": "935215385971970049",
+					"source_user_id": 15182368,
+					"source_user_id_str": "15182368"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson, but now with added tenure",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 1980s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								74,
+								97
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1130,
+			"friends_count": 19,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 14916,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 49728,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925912547038969856/51DKTe_r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925912547038969856/51DKTe_r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Nov 27 18:34:58 +0000 2017",
+			"id": 935215385971970000,
+			"id_str": "935215385971970049",
+			"text": "OH MY DEAR GOD https://t.co/7oL58M5KXv",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 935215379340845000,
+						"id_str": "935215379340845056",
+						"indices": [
+							15,
+							38
+						],
+						"media_url": "http://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+						"url": "https://t.co/7oL58M5KXv",
+						"display_url": "pic.twitter.com/7oL58M5KXv",
+						"expanded_url": "https://twitter.com/alexdecampi/status/935215385971970049/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 750,
+								"h": 942,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 541,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 942,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 935215379340845000,
+						"id_str": "935215379340845056",
+						"indices": [
+							15,
+							38
+						],
+						"media_url": "http://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DPqNbroX4AA-7T_.jpg",
+						"url": "https://t.co/7oL58M5KXv",
+						"display_url": "pic.twitter.com/7oL58M5KXv",
+						"expanded_url": "https://twitter.com/alexdecampi/status/935215385971970049/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 750,
+								"h": 942,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 541,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 942,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15182368,
+				"id_str": "15182368",
+				"name": "Alex de Campi",
+				"screen_name": "alexdecampi",
+				"location": "New York City",
+				"description": "Writer of Mayday, No Mercy, Bankshot, Semiautomagic, Archie vs Predator, Judge Dredd, Ghost in the Shell, Wonder Woman and your momma.",
+				"url": "https://t.co/lPSLsjRn09",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lPSLsjRn09",
+								"expanded_url": "http://www.alexdecampi.com",
+								"display_url": "alexdecampi.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 13909,
+				"friends_count": 1870,
+				"listed_count": 305,
+				"created_at": "Fri Jun 20 17:53:01 +0000 2008",
+				"favourites_count": 29006,
+				"utc_offset": -18000,
+				"time_zone": "Quito",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 49340,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000401020627/6a58ea1452ebd15381fa0542e5eb4592_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000401020627/6a58ea1452ebd15381fa0542e5eb4592_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15182368/1462986263",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "A8C7F7",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1293,
+			"favorite_count": 3098,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1293,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Nov 28 23:21:01 +0000 2017",
 		"id": 935649763357352000,
 		"id_str": "935649763357351942",
