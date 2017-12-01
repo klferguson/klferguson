e930312a09eef1d6724f8dc5738e7a0906ff846a
klferguson <klferguson@users.noreply.github.com>
2017-11-30 21:12:30 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 49868,
-	"created_at": "Fri Dec 01 00:12:28 +0000 2017",
+	"tweets": 49869,
+	"created_at": "Fri Dec 01 02:12:29 +0000 2017",
 	"lang": "en"
 }
---
+++ data/js/tweets/2017_12.js
@@ -0,0 +1,294 @@
+Grailbird.data.tweets_2017_12 = 
+[
+	{
+		"created_at": "Fri Dec 01 02:10:15 +0000 2017",
+		"id": 936417126839210000,
+		"id_str": "936417126839209986",
+		"text": "i do this with my arms all the time. https://t.co/kG3kympuW4",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kG3kympuW4",
+					"expanded_url": "https://twitter.com/willow_catelyn/status/936192196423794689",
+					"display_url": "twitter.com/willow_catelyn…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "Kevin L. Ferguson, but now with added tenure",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 1980s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								74,
+								97
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1130,
+			"friends_count": 461,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 14937,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 49772,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925912547038969856/51DKTe_r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925912547038969856/51DKTe_r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 936192196423794700,
+		"quoted_status_id_str": "936192196423794689",
+		"quoted_status": {
+			"created_at": "Thu Nov 30 11:16:27 +0000 2017",
+			"id": 936192196423794700,
+			"id_str": "936192196423794689",
+			"text": "For your Consideration: Adam Driver's arms https://t.co/Et2BVlydsb",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 936192161380487200,
+						"id_str": "936192161380487168",
+						"indices": [
+							43,
+							66
+						],
+						"media_url": "http://pbs.twimg.com/media/DP4Fz4SW0AAfuAH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DP4Fz4SW0AAfuAH.jpg",
+						"url": "https://t.co/Et2BVlydsb",
+						"display_url": "pic.twitter.com/Et2BVlydsb",
+						"expanded_url": "https://twitter.com/willow_catelyn/status/936192196423794689/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 763,
+								"h": 1000,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 763,
+								"h": 1000,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 519,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 936192161380487200,
+						"id_str": "936192161380487168",
+						"indices": [
+							43,
+							66
+						],
+						"media_url": "http://pbs.twimg.com/media/DP4Fz4SW0AAfuAH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DP4Fz4SW0AAfuAH.jpg",
+						"url": "https://t.co/Et2BVlydsb",
+						"display_url": "pic.twitter.com/Et2BVlydsb",
+						"expanded_url": "https://twitter.com/willow_catelyn/status/936192196423794689/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 763,
+								"h": 1000,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 763,
+								"h": 1000,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 519,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 924450967,
+				"id_str": "924450967",
+				"name": "Willow Catelyn Maclay",
+				"screen_name": "willow_catelyn",
+				"location": "St. John's. Canada",
+				"description": "Writer/Film Critic. Village Voice, Ebert Voices, Cleo Journal, Film Stage. Calcifer's mom. Erin, but with different hair. Contact: willowcatelynmaclay@gmail",
+				"url": "https://t.co/N1kXLZC4pX",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/N1kXLZC4pX",
+								"expanded_url": "http://curtsiesandhandgrenades.blogspot.com/",
+								"display_url": "curtsiesandhandgrenades.blogspot.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2623,
+				"friends_count": 1129,
+				"listed_count": 77,
+				"created_at": "Sun Nov 04 02:01:46 +0000 2012",
+				"favourites_count": 38218,
+				"utc_offset": -14400,
+				"time_zone": "Atlantic Time (Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 62220,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "3B94D9",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/625360519281537024/aX7-zlss.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/625360519281537024/aX7-zlss.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/926113605082284032/IvjN5rSk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/926113605082284032/IvjN5rSk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/924450967/1502213863",
+				"profile_link_color": "9266CC",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 31,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
