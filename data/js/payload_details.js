038a55d61c8f668a1110dc633dd1e10bdde9f94a
klferguson <klferguson@users.noreply.github.com>
2018-01-03 19:12:23 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50596,
-	"created_at": "Wed Jan 03 21:12:22 +0000 2018",
+	"tweets": 50597,
+	"created_at": "Thu Jan 04 00:12:21 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Wed Jan 03 23:53:47 +0000 2018",
+		"id": 948703970058539000,
+		"id_str": "948703970058539008",
+		"text": "RT @onwardolympians: Slint have really let themselves go. https://t.co/SPMXCcKlXU",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "onwardolympians",
+					"name": "Olympians",
+					"id": 118893267,
+					"id_str": "118893267",
+					"indices": [
+						3,
+						19
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 948664091496910800,
+					"id_str": "948664091496910848",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+					"url": "https://t.co/SPMXCcKlXU",
+					"display_url": "pic.twitter.com/SPMXCcKlXU",
+					"expanded_url": "https://twitter.com/onwardolympians/status/948664226373095426/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 948664226373095400,
+					"source_status_id_str": "948664226373095426",
+					"source_user_id": 118893267,
+					"source_user_id_str": "118893267"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 948664091496910800,
+					"id_str": "948664091496910848",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+					"url": "https://t.co/SPMXCcKlXU",
+					"display_url": "pic.twitter.com/SPMXCcKlXU",
+					"expanded_url": "https://twitter.com/onwardolympians/status/948664226373095426/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 948664226373095400,
+					"source_status_id_str": "948664226373095426",
+					"source_user_id": 118893267,
+					"source_user_id_str": "118893267"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "still kevin, still not marguerite duras",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 1980s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								74,
+								97
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1127,
+			"friends_count": 459,
+			"listed_count": 45,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 15272,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 50492,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/945503335460646913/bHeCwDXU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/945503335460646913/bHeCwDXU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jan 03 21:15:51 +0000 2018",
+			"id": 948664226373095400,
+			"id_str": "948664226373095426",
+			"text": "Slint have really let themselves go. https://t.co/SPMXCcKlXU",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 948664091496910800,
+						"id_str": "948664091496910848",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+						"url": "https://t.co/SPMXCcKlXU",
+						"display_url": "pic.twitter.com/SPMXCcKlXU",
+						"expanded_url": "https://twitter.com/onwardolympians/status/948664226373095426/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 948664091496910800,
+						"id_str": "948664091496910848",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DSpU9wjXkAAqldu.jpg",
+						"url": "https://t.co/SPMXCcKlXU",
+						"display_url": "pic.twitter.com/SPMXCcKlXU",
+						"expanded_url": "https://twitter.com/onwardolympians/status/948664226373095426/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 118893267,
+				"id_str": "118893267",
+				"name": "Olympians",
+				"screen_name": "onwardolympians",
+				"location": "London, England",
+				"description": "Whelmed by it all. Debut album Reasons to be Tearful out now.",
+				"url": "https://t.co/Th8Rm9ZN2S",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Th8Rm9ZN2S",
+								"expanded_url": "http://www.onwardolympians.com",
+								"display_url": "onwardolympians.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1456,
+				"friends_count": 1652,
+				"listed_count": 27,
+				"created_at": "Tue Mar 02 01:29:42 +0000 2010",
+				"favourites_count": 3149,
+				"utc_offset": 3600,
+				"time_zone": "Amsterdam",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 11155,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "333333",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/849814008/1af43cb786aea3137f5b903b5c17c9d4.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/849814008/1af43cb786aea3137f5b903b5c17c9d4.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/780477210650116097/_xYK21N-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/780477210650116097/_xYK21N-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/118893267/1500464115",
+				"profile_link_color": "FFCC4D",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "F6F6F6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 69,
+			"favorite_count": 186,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 69,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jan 03 20:14:27 +0000 2018",
 		"id": 948648772413919200,
 		"id_str": "948648772413919233",
