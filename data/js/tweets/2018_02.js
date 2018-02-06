cc5ab2d78652c6c040cc2e37c885735c65703160
klferguson <klferguson@users.noreply.github.com>
2018-02-06 08:12:24 -0500

    Added 2 tweets

--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,567 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Tue Feb 06 12:50:44 +0000 2018",
+		"id": 960858297950789600,
+		"id_str": "960858297950789640",
+		"text": "RT @QCVoices: Like classic art memes? So do we. Stay tuned for more from our resident memeologist Miriam. https://t.co/dIvpRk0lln",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "QCVoices",
+					"name": "QC Voices",
+					"id": 1915199772,
+					"id_str": "1915199772",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 960855180794200000,
+					"id_str": "960855180794200074",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+					"url": "https://t.co/dIvpRk0lln",
+					"display_url": "pic.twitter.com/dIvpRk0lln",
+					"expanded_url": "https://twitter.com/QCVoices/status/960855452228648961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 654,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 758,
+							"h": 729,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 758,
+							"h": 729,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 960855452228649000,
+					"source_status_id_str": "960855452228648961",
+					"source_user_id": 1915199772,
+					"source_user_id_str": "1915199772"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 960855180794200000,
+					"id_str": "960855180794200074",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+					"url": "https://t.co/dIvpRk0lln",
+					"display_url": "pic.twitter.com/dIvpRk0lln",
+					"expanded_url": "https://twitter.com/QCVoices/status/960855452228648961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 654,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 758,
+							"h": 729,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 758,
+							"h": 729,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 960855452228649000,
+					"source_status_id_str": "960855452228648961",
+					"source_user_id": 1915199772,
+					"source_user_id_str": "1915199772"
+				},
+				{
+					"id": 960855180836184000,
+					"id_str": "960855180836184064",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DVWksjZVoAAm8FB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVWksjZVoAAm8FB.jpg",
+					"url": "https://t.co/dIvpRk0lln",
+					"display_url": "pic.twitter.com/dIvpRk0lln",
+					"expanded_url": "https://twitter.com/QCVoices/status/960855452228648961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 424,
+							"h": 653,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 424,
+							"h": 653,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 424,
+							"h": 653,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 960855452228649000,
+					"source_status_id_str": "960855452228648961",
+					"source_user_id": 1915199772,
+					"source_user_id_str": "1915199772"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kevin, who hates bullies, most especially administ",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 1980s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								74,
+								97
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1134,
+			"friends_count": 461,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 15520,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51316,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/945503335460646913/bHeCwDXU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/945503335460646913/bHeCwDXU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Feb 06 12:39:26 +0000 2018",
+			"id": 960855452228649000,
+			"id_str": "960855452228648961",
+			"text": "Like classic art memes? So do we. Stay tuned for more from our resident memeologist Miriam. https://t.co/dIvpRk0lln",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 960855180794200000,
+						"id_str": "960855180794200074",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+						"url": "https://t.co/dIvpRk0lln",
+						"display_url": "pic.twitter.com/dIvpRk0lln",
+						"expanded_url": "https://twitter.com/QCVoices/status/960855452228648961/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 654,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 758,
+								"h": 729,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 758,
+								"h": 729,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 960855180794200000,
+						"id_str": "960855180794200074",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVWksjPVAAoGBU1.jpg",
+						"url": "https://t.co/dIvpRk0lln",
+						"display_url": "pic.twitter.com/dIvpRk0lln",
+						"expanded_url": "https://twitter.com/QCVoices/status/960855452228648961/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 654,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 758,
+								"h": 729,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 758,
+								"h": 729,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 960855180836184000,
+						"id_str": "960855180836184064",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/DVWksjZVoAAm8FB.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVWksjZVoAAm8FB.jpg",
+						"url": "https://t.co/dIvpRk0lln",
+						"display_url": "pic.twitter.com/dIvpRk0lln",
+						"expanded_url": "https://twitter.com/QCVoices/status/960855452228648961/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 424,
+								"h": 653,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 424,
+								"h": 653,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 424,
+								"h": 653,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1915199772,
+				"id_str": "1915199772",
+				"name": "QC Voices",
+				"screen_name": "QCVoices",
+				"location": "Queens College, CUNY",
+				"description": "What happens when you ask a handful of Queens College students to collaboratively write about their passions?  Visit QC Voices and find out.",
+				"url": "http://t.co/qgk23M9NZL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/qgk23M9NZL",
+								"expanded_url": "http://qcvoices.qwriting.qc.cuny.edu",
+								"display_url": "qcvoices.qwriting.qc.cuny.edu",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 191,
+				"friends_count": 806,
+				"listed_count": 3,
+				"created_at": "Sat Sep 28 20:25:48 +0000 2013",
+				"favourites_count": 92,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 227,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/566250538459598848/i-AZO1bR_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/566250538459598848/i-AZO1bR_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1915199772/1423839036",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Tue Feb 06 12:40:48 +0000 2018",
+		"id": 960855796266434600,
+		"id_str": "960855796266434560",
+		"text": "on today's agenda: *The Graduate voice* spreadsheets",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kevin, who hates bullies, most especially administ",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 1980s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								74,
+								97
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1134,
+			"friends_count": 461,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 15520,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51316,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/403882578/the_typecast.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/945503335460646913/bHeCwDXU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/945503335460646913/bHeCwDXU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Feb 06 04:26:59 +0000 2018",
 		"id": 960731525758308400,
 		"id_str": "960731525758308352",
