b6049a711d883e9f61595b68f3a5ad4018564daf
klferguson <klferguson@users.noreply.github.com>
2018-03-26 13:12:32 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53042,
-	"created_at": "Mon Mar 26 16:12:31 +0000 2018",
+	"tweets": 53043,
+	"created_at": "Mon Mar 26 17:12:31 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_03.js",
 		"year": 2018,
 		"var_name": "tweets_2018_03",
-		"tweet_count": 1028,
+		"tweet_count": 1029,
 		"month": 3
 	},
 	{
