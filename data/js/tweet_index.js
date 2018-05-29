5d1a0c34abc1a52dd2eb25857d6e549fd93f635e
klferguson <klferguson@users.noreply.github.com>
2018-05-29 01:12:29 -0400

    Added 4 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_05.js",
 		"year": 2018,
 		"var_name": "tweets_2018_05",
-		"tweet_count": 787,
+		"tweet_count": 791,
 		"month": 5
 	},
 	{
