e419359dd1decef74838c8d7bb2526ec14adac23
klferguson <klferguson@users.noreply.github.com>
2018-08-23 19:12:28 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56254,
-	"created_at": "Thu Aug 23 21:12:25 +0000 2018",
+	"tweets": 56255,
+	"created_at": "Thu Aug 23 23:12:27 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Thu Aug 23 23:10:48 +0000 2018",
+		"id": 1032767138178760700,
+		"id_str": "1032767138178760707",
+		"text": "RT @_TonyHilfiger: when them Attachment: 3 images is screenshots of you wylin on the timeline instead of nudes https://t.co/W3yZq5L5X8",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_TonyHilfiger",
+					"name": "a pimp named slickback",
+					"id": 416907282,
+					"id_str": "416907282",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1032546567293739000,
+					"id_str": "1032546567293739008",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+					"url": "https://t.co/W3yZq5L5X8",
+					"display_url": "pic.twitter.com/W3yZq5L5X8",
+					"expanded_url": "https://twitter.com/_TonyHilfiger/status/1032546586654650369/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 552,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 952,
+							"h": 1172,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 952,
+							"h": 1172,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1032546586654650400,
+					"source_status_id_str": "1032546586654650369",
+					"source_user_id": 416907282,
+					"source_user_id_str": "416907282"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1032546567293739000,
+					"id_str": "1032546567293739008",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+					"url": "https://t.co/W3yZq5L5X8",
+					"display_url": "pic.twitter.com/W3yZq5L5X8",
+					"expanded_url": "https://twitter.com/_TonyHilfiger/status/1032546586654650369/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 552,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 952,
+							"h": 1172,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 952,
+							"h": 1172,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1032546586654650400,
+					"source_status_id_str": "1032546586654650369",
+					"source_user_id": 416907282,
+					"source_user_id_str": "416907282"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kevin, quasimodo emo wheelchair",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 80s/90s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								76,
+								99
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								103,
+								126
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1162,
+			"friends_count": 468,
+			"listed_count": 50,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 17963,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 56034,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1030122229248348162/peyi7Zyz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1030122229248348162/peyi7Zyz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Aug 23 08:34:24 +0000 2018",
+			"id": 1032546586654650400,
+			"id_str": "1032546586654650369",
+			"text": "when them Attachment: 3 images is screenshots of you wylin on the timeline instead of nudes https://t.co/W3yZq5L5X8",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1032546567293739000,
+						"id_str": "1032546567293739008",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+						"url": "https://t.co/W3yZq5L5X8",
+						"display_url": "pic.twitter.com/W3yZq5L5X8",
+						"expanded_url": "https://twitter.com/_TonyHilfiger/status/1032546586654650369/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 552,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 952,
+								"h": 1172,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 952,
+								"h": 1172,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1032546567293739000,
+						"id_str": "1032546567293739008",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DlRXod2XoAAdBv8.jpg",
+						"url": "https://t.co/W3yZq5L5X8",
+						"display_url": "pic.twitter.com/W3yZq5L5X8",
+						"expanded_url": "https://twitter.com/_TonyHilfiger/status/1032546586654650369/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 552,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 952,
+								"h": 1172,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 952,
+								"h": 1172,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 416907282,
+				"id_str": "416907282",
+				"name": "a pimp named slickback",
+				"screen_name": "_TonyHilfiger",
+				"location": "South Carolina, USA",
+				"description": "im like a young richard pryor with unforgettable quotes. they only care about a nigga when he tellin a joke",
+				"url": "https://t.co/iDTizRrLR4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/iDTizRrLR4",
+								"expanded_url": "https://twitter.com/_tonyhilfiger/favorites",
+								"display_url": "twitter.com/_tonyhilfiger/…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5051,
+				"friends_count": 822,
+				"listed_count": 273,
+				"created_at": "Sun Nov 20 08:47:10 +0000 2011",
+				"favourites_count": 668,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 200677,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "525252",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1026780596360699904/awY4YWDT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1026780596360699904/awY4YWDT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/416907282/1533184725",
+				"profile_link_color": "B80722",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "616161",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1798,
+			"favorite_count": 3481,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1798,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Aug 23 20:43:51 +0000 2018",
 		"id": 1032730158418026500,
 		"id_str": "1032730158418026497",
