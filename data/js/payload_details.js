1668e86bb71266458f58c7269df82192fbf0525a
klferguson <klferguson@users.noreply.github.com>
2018-09-04 23:12:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56454,
-	"created_at": "Wed Sep 05 02:12:23 +0000 2018",
+	"tweets": 56455,
+	"created_at": "Wed Sep 05 03:12:21 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Wed Sep 05 03:06:44 +0000 2018",
+		"id": 1037175166571356200,
+		"id_str": "1037175166571356160",
+		"text": "RT @MOONEMOTlCON: Friend: “it’s still only Septemb-\n\nMe:🖤🎃🍂🕷🔮🕸🌙👁🦇🔪👣⚰️💀🖤🕷🎃🍂🕸🌙👁🦇🕷💀🖤🎃🎃🎃🖤🕷🕸⚰️🦇👁🖤💀🕸🌙🍂⚰️⚰️⚰️🔮🍂🍂🍂🔮🔮🎃🖤🖤🕸💀👁🔪👣💀🖤🍂🎃 🕸💀👁🔪💀🖤🍂🎃🔮🕸🌙👁🦇🕷💀⚰️🖤…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MOONEMOTlCON",
+					"name": "𖤐₆⁶₆𖤐",
+					"id": 1653179628,
+					"id_str": "1653179628",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kevin, get[ting] on this tv mama",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 80s/90s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								76,
+								99
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								103,
+								126
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1160,
+			"friends_count": 468,
+			"listed_count": 51,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 18042,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 56234,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1033138104201170944/EVq2zjs4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1033138104201170944/EVq2zjs4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Sep 03 20:45:28 +0000 2018",
+			"id": 1036716831946498000,
+			"id_str": "1036716831946498049",
+			"text": "Friend: “it’s still only Septemb-\n\nMe:🖤🎃🍂🕷🔮🕸🌙👁🦇🔪👣⚰️💀🖤🕷🎃🍂🕸🌙👁🦇🕷💀🖤🎃🎃🎃🖤🕷🕸⚰️🦇👁🖤💀🕸🌙🍂⚰️⚰️⚰️🔮🍂🍂🍂🔮🔮🎃🖤🖤🕸💀👁🔪👣💀🖤🍂🎃 🕸💀👁🔪💀🖤🍂🎃🔮🕸🌙👁… https://t.co/MXxZWUupCq",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/MXxZWUupCq",
+						"expanded_url": "https://twitter.com/i/web/status/1036716831946498049",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1653179628,
+				"id_str": "1653179628",
+				"name": "𖤐₆⁶₆𖤐",
+				"screen_name": "MOONEMOTlCON",
+				"location": " lana del rey stan",
+				"description": "retired thotamus prime",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10460,
+				"friends_count": 11106,
+				"listed_count": 67,
+				"created_at": "Wed Aug 07 15:24:39 +0000 2013",
+				"favourites_count": 14149,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1127,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1029862361275019265/gYkV5Cmu_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1029862361275019265/gYkV5Cmu_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1653179628/1532388819",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 67622,
+			"favorite_count": 160409,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 67622,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Sep 05 01:25:25 +0000 2018",
 		"id": 1037149669942542300,
 		"id_str": "1037149669942542337",
