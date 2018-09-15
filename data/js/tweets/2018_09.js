816b35dc7f3b1865a2fc7f3665f30ae89b15c6bd
klferguson <klferguson@users.noreply.github.com>
2018-09-15 18:12:25 -0400

    Added 1 tweets

--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,468 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Sat Sep 15 21:31:38 +0000 2018",
+		"id": 1041077102760652800,
+		"id_str": "1041077102760652803",
+		"text": "RT @cinebeats: Story of my life . . . https://t.co/ruVxnpAXiS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cinebeats",
+					"name": "Kimberly Lindbergs",
+					"id": 20551430,
+					"id_str": "20551430",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1041049109140955100,
+					"id_str": "1041049109140955136",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+					"url": "https://t.co/ruVxnpAXiS",
+					"display_url": "pic.twitter.com/ruVxnpAXiS",
+					"expanded_url": "https://twitter.com/cinebeats/status/1041051310089371648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 711,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 711,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 430,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1041051310089371600,
+					"source_status_id_str": "1041051310089371648",
+					"source_user_id": 20551430,
+					"source_user_id_str": "20551430"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1041049109140955100,
+					"id_str": "1041049109140955136",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+					"url": "https://t.co/ruVxnpAXiS",
+					"display_url": "pic.twitter.com/ruVxnpAXiS",
+					"expanded_url": "https://twitter.com/cinebeats/status/1041051310089371648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 711,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 711,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 430,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1041051310089371600,
+					"source_status_id_str": "1041051310089371648",
+					"source_user_id": 20551430,
+					"source_user_id_str": "20551430"
+				},
+				{
+					"id": 1041049126438228000,
+					"id_str": "1041049126438227968",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/media/DnKMqi8U0AAt4HV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnKMqi8U0AAt4HV.jpg",
+					"url": "https://t.co/ruVxnpAXiS",
+					"display_url": "pic.twitter.com/ruVxnpAXiS",
+					"expanded_url": "https://twitter.com/cinebeats/status/1041051310089371648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 711,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 430,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 711,
+							"h": 450,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1041051310089371600,
+					"source_status_id_str": "1041051310089371648",
+					"source_user_id": 20551430,
+					"source_user_id_str": "20551430"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kevin, with a side of conventional cute",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "DH + media studies, 80s/90s, Acker, horror • English, Queens College/CUNY • https://t.co/vKRCFmkjP0… • https://t.co/nMrHfp2VtD",
+			"url": "https://t.co/DlQKTr96fQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DlQKTr96fQ",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vKRCFmkjP0",
+							"expanded_url": "http://palgrave.com/us/book/978113",
+							"display_url": "palgrave.com/us/book/978113",
+							"indices": [
+								76,
+								99
+							]
+						},
+						{
+							"url": "https://t.co/nMrHfp2VtD",
+							"expanded_url": "http://english.qc.cuny.edu/klferguson/",
+							"display_url": "english.qc.cuny.edu/klferguson/",
+							"indices": [
+								103,
+								126
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1160,
+			"friends_count": 471,
+			"listed_count": 51,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 18137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 56448,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1040346421902430213/axI_BkJQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1040346421902430213/axI_BkJQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Sep 15 19:49:08 +0000 2018",
+			"id": 1041051310089371600,
+			"id_str": "1041051310089371648",
+			"text": "Story of my life . . . https://t.co/ruVxnpAXiS",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1041049109140955100,
+						"id_str": "1041049109140955136",
+						"indices": [
+							23,
+							46
+						],
+						"media_url": "http://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+						"url": "https://t.co/ruVxnpAXiS",
+						"display_url": "pic.twitter.com/ruVxnpAXiS",
+						"expanded_url": "https://twitter.com/cinebeats/status/1041051310089371648/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 711,
+								"h": 450,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 711,
+								"h": 450,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 430,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1041049109140955100,
+						"id_str": "1041049109140955136",
+						"indices": [
+							23,
+							46
+						],
+						"media_url": "http://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DnKMpigVYAAcpET.jpg",
+						"url": "https://t.co/ruVxnpAXiS",
+						"display_url": "pic.twitter.com/ruVxnpAXiS",
+						"expanded_url": "https://twitter.com/cinebeats/status/1041051310089371648/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 711,
+								"h": 450,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 711,
+								"h": 450,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 430,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1041049126438228000,
+						"id_str": "1041049126438227968",
+						"indices": [
+							23,
+							46
+						],
+						"media_url": "http://pbs.twimg.com/media/DnKMqi8U0AAt4HV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DnKMqi8U0AAt4HV.jpg",
+						"url": "https://t.co/ruVxnpAXiS",
+						"display_url": "pic.twitter.com/ruVxnpAXiS",
+						"expanded_url": "https://twitter.com/cinebeats/status/1041051310089371648/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 711,
+								"h": 450,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 430,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 711,
+								"h": 450,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20551430,
+				"id_str": "20551430",
+				"name": "Kimberly Lindbergs",
+				"screen_name": "cinebeats",
+				"location": "Napa, CA",
+				"description": "Writer, artist & @AWFJ member. Currently working for @FilmStruck, @TCM & @PublishersWkly. Opinions are my own. More @ https://t.co/UW9hKOQMse",
+				"url": "https://t.co/x6oa1tSZoT",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/x6oa1tSZoT",
+								"expanded_url": "http://www.cinebeats.com",
+								"display_url": "cinebeats.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/UW9hKOQMse",
+								"expanded_url": "http://kimberlylindbergs.com",
+								"display_url": "kimberlylindbergs.com",
+								"indices": [
+									118,
+									141
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 3036,
+				"friends_count": 1147,
+				"listed_count": 229,
+				"created_at": "Tue Feb 10 22:10:33 +0000 2009",
+				"favourites_count": 12831,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 13007,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1039409640725860352/L31-DISt_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1039409640725860352/L31-DISt_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20551430/1516951050",
+				"profile_link_color": "FF691F",
+				"profile_sidebar_border_color": "99CC99",
+				"profile_sidebar_fill_color": "66CCCC",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Sep 15 20:31:57 +0000 2018",
 		"id": 1041062082010079200,
 		"id_str": "1041062082010079233",
