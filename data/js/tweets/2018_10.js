f81eda27c5916d8f73937752fcced6390523c624
klferguson <klferguson@users.noreply.github.com>
2018-10-31 14:12:30 -0400

    Added 1 tweets

--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Wed Oct 31 15:33:05 +0000 2018",
+		"id": 1057656714781175800,
+		"id_str": "1057656714781175808",
+		"full_text": "Someone at the DMV in Utah dressed as a Zootopia sloth so that’s it, they won Halloween and the rest of y’all can give up now https://t.co/vPc6j6zdXL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1057656710456737800,
+					"id_str": "1057656710456737792",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq2NLKkVAAAIqh6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq2NLKkVAAAIqh6.jpg",
+					"url": "https://t.co/vPc6j6zdXL",
+					"display_url": "pic.twitter.com/vPc6j6zdXL",
+					"expanded_url": "https://twitter.com/officerdaddo/status/1057656714781175808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1096,
+							"h": 531,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1096,
+							"h": 531,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 329,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1057656710456737800,
+					"id_str": "1057656710456737792",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq2NLKkVAAAIqh6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq2NLKkVAAAIqh6.jpg",
+					"url": "https://t.co/vPc6j6zdXL",
+					"display_url": "pic.twitter.com/vPc6j6zdXL",
+					"expanded_url": "https://twitter.com/officerdaddo/status/1057656714781175808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1096,
+							"h": 531,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1096,
+							"h": 531,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 329,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 974085552776581100,
+			"id_str": "974085552776581120",
+			"name": "Count DracuDaddo",
+			"screen_name": "officerdaddo",
+			"location": "United States",
+			"description": "Chasin perps in fast-forward but we cuff in slo-mo.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3360,
+			"friends_count": 456,
+			"listed_count": 13,
+			"created_at": "Thu Mar 15 00:51:08 +0000 2018",
+			"favourites_count": 16148,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12580,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/987765508631121920/5UBWsV1m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/987765508631121920/5UBWsV1m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/974085552776581120/1540616265",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 111,
+		"favorite_count": 442,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Someone at the DMV in Utah dressed as a Zootopia sloth so that’s it, they won Halloween and the rest of y’all can give up now https://t.co/vPc6j6zdXL"
+	},
+	{
 		"created_at": "Wed Oct 31 16:58:19 +0000 2018",
 		"id": 1057678164837699600,
 		"id_str": "1057678164837699584",
