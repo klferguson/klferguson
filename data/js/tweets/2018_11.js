71f0fb156155bb407f6843b8e5cdff4624df61ae
klferguson <klferguson@users.noreply.github.com>
2018-11-06 14:12:24 -0500

    Added 1 tweets

--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Tue Nov 06 18:15:56 +0000 2018",
+		"id": 1059872025395703800,
+		"id_str": "1059872025395703808",
+		"full_text": "mine was the first ballot cast at my polling place this morning in my pedro town! https://t.co/yl4N853J9D",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1059871982869606400,
+					"id_str": "1059871982869606400",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DrVr8_ZU8AAolZ_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrVr8_ZU8AAolZ_.jpg",
+					"url": "https://t.co/yl4N853J9D",
+					"display_url": "pic.twitter.com/yl4N853J9D",
+					"expanded_url": "https://twitter.com/wattfrompedro/status/1059872025395703808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1059871982869606400,
+					"id_str": "1059871982869606400",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DrVr8_ZU8AAolZ_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrVr8_ZU8AAolZ_.jpg",
+					"url": "https://t.co/yl4N853J9D",
+					"display_url": "pic.twitter.com/yl4N853J9D",
+					"expanded_url": "https://twitter.com/wattfrompedro/status/1059872025395703808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1059872002356342800,
+					"id_str": "1059872002356342784",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DrVr-H_U8AAkCQ6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrVr-H_U8AAkCQ6.jpg",
+					"url": "https://t.co/yl4N853J9D",
+					"display_url": "pic.twitter.com/yl4N853J9D",
+					"expanded_url": "https://twitter.com/wattfrompedro/status/1059872025395703808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 612,
+							"h": 612,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19487826,
+			"id_str": "19487826",
+			"name": "mike watt",
+			"screen_name": "wattfrompedro",
+			"location": "san pedro, ca, us",
+			"description": "I'm the mike watt who lives in pedro and works the bass",
+			"url": "https://t.co/cZg85uWJvV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cZg85uWJvV",
+							"expanded_url": "http://mikewatt.com/",
+							"display_url": "mikewatt.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34371,
+			"friends_count": 18,
+			"listed_count": 620,
+			"created_at": "Sun Jan 25 13:24:41 +0000 2009",
+			"favourites_count": 10,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2864,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFCC00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/294082369/watt_for_twitter_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/294082369/watt_for_twitter_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19487826/1410044171",
+			"profile_link_color": "20751C",
+			"profile_sidebar_border_color": "FFCC00",
+			"profile_sidebar_fill_color": "FFCC00",
+			"profile_text_color": "330000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 27,
+		"favorite_count": 245,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "mine was the first ballot cast at my polling place this morning in my pedro town! https://t.co/yl4N853J9D"
+	},
+	{
 		"created_at": "Tue Nov 06 17:23:55 +0000 2018",
 		"id": 1059858934226411500,
 		"id_str": "1059858934226411520",
