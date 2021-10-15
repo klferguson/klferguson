45822fd6b37cf9dbe8216fe7ebd67422dab353e4
klferguson <klferguson@users.noreply.github.com>
2021-10-15 04:10:38 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 104728,
-	"created_at": "Thu Oct 14 08:10:36 +0000 2021",
+	"tweets": 104729,
+	"created_at": "Fri Oct 15 08:10:38 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 14 21:20:07 +0000 2021",
+		"id": 1448760553074274300,
+		"id_str": "1448760553074274309",
+		"full_text": "https://t.co/rT9AlneyWU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1448760540680048600,
+					"id_str": "1448760540680048650",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FBsICS_WUAoylZM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBsICS_WUAoylZM.jpg",
+					"url": "https://t.co/rT9AlneyWU",
+					"display_url": "pic.twitter.com/rT9AlneyWU",
+					"expanded_url": "https://twitter.com/KevinLFerguson/status/1448760553074274309/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1448760540680048600,
+					"id_str": "1448760540680048650",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FBsICS_WUAoylZM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBsICS_WUAoylZM.jpg",
+					"url": "https://t.co/rT9AlneyWU",
+					"display_url": "pic.twitter.com/rT9AlneyWU",
+					"expanded_url": "https://twitter.com/KevinLFerguson/status/1448760553074274309/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kev, tattoo of a bee wearing a ghost costume 🐝 👻",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "🫀",
+			"url": "https://t.co/CZSN5IiorI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CZSN5IiorI",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1273,
+			"friends_count": 557,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 26918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 85564,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1444387392237051936/MI9MdCbz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1444387392237051936/MI9MdCbz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/rT9AlneyWU"
+	},
+	{
 		"created_at": "Wed Oct 13 19:08:20 +0000 2021",
 		"id": 1448365003963347000,
 		"id_str": "1448365003963346949",
@@ -207,7 +382,7 @@ Grailbird.data.tweets_2021_10 =
 		"created_at": "Wed Oct 13 15:29:28 +0000 2021",
 		"id": 1448309922375012400,
 		"id_str": "1448309922375012352",
-		"full_text": "@samplereality 🧟‍♂️ Dark Trample 🧟‍♂️",
+		"full_text": "@samplereality 🧟♂️ Dark Trample 🧟♂️",
 		"truncated": false,
 		"display_text_range": [
 			15,
@@ -308,7 +483,7 @@ Grailbird.data.tweets_2021_10 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@samplereality 🧟‍♂️ Dark Trample 🧟‍♂️"
+		"text": "@samplereality 🧟♂️ Dark Trample 🧟♂️"
 	},
 	{
 		"created_at": "Wed Oct 13 14:03:54 +0000 2021",
