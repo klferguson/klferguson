d5f944288ed9f27cd77a5429d567d9a70ac3a9c9
klferguson <klferguson@users.noreply.github.com>
2021-11-06 04:10:35 -0400

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 105304,
-	"created_at": "Fri Nov 05 08:10:36 +0000 2021",
+	"tweets": 105306,
+	"created_at": "Sat Nov 06 08:10:35 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Fri Nov 05 20:17:38 +0000 2021",
+		"id": 1456717363345252400,
+		"id_str": "1456717363345252366",
+		"full_text": "just winked at the guy at the covid testing site who was trying to offer me hand sanitizer?? 🙄🙄",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 359023788,
+			"id_str": "359023788",
+			"name": "kev, non-strategic upskilling",
+			"screen_name": "KevinLFerguson",
+			"location": "Brooklyn",
+			"description": "🫀",
+			"url": "https://t.co/CZSN5IiWhg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CZSN5IiWhg",
+							"expanded_url": "http://filmvis.tumblr.com",
+							"display_url": "filmvis.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1276,
+			"friends_count": 561,
+			"listed_count": 46,
+			"created_at": "Sat Aug 20 22:13:23 +0000 2011",
+			"favourites_count": 27148,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 86159,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1455154082759561228/yCGsvYgR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1455154082759561228/yCGsvYgR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/359023788/1462625620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "just winked at the guy at the covid testing site who was trying to offer me hand sanitizer?? 🙄🙄"
+	},
+	{
+		"created_at": "Fri Nov 05 13:10:48 +0000 2021",
+		"id": 1456420377127755800,
+		"id_str": "1456609946813018115",
+		"full_text": "https://t.co/qROuktiV4k",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1456420360023330800,
+					"id_str": "1456420360023330818",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FDY-mdrWUAIXXGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDY-mdrWUAIXXGi.jpg",
+					"url": "https://t.co/qROuktiV4k",
+					"display_url": "pic.twitter.com/qROuktiV4k",
+					"expanded_url": "https://twitter.com/BrettRedacted/status/1456420377127755777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1170,
+							"h": 1170,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1170,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1456420360023330800,
+					"id_str": "1456420360023330818",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FDY-mdrWUAIXXGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDY-mdrWUAIXXGi.jpg",
+					"url": "https://t.co/qROuktiV4k",
+					"display_url": "pic.twitter.com/qROuktiV4k",
+					"expanded_url": "https://twitter.com/BrettRedacted/status/1456420377127755777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1170,
+							"h": 1170,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1170,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1456420372564385800,
+					"id_str": "1456420372564385798",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FDY-nMZXoAY7AWB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDY-nMZXoAY7AWB.jpg",
+					"url": "https://t.co/qROuktiV4k",
+					"display_url": "pic.twitter.com/qROuktiV4k",
+					"expanded_url": "https://twitter.com/BrettRedacted/status/1456420377127755777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 499,
+							"h": 334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 334,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 499,
+							"h": 334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18511844,
+			"id_str": "18511844",
+			"name": "Brett ________",
+			"screen_name": "BrettRedacted",
+			"location": "NYC via Chicago suburbs",
+			"description": "@TheNewFlesh host, 🍅-approved movie opinions. @queenchill’s husband. stand-up comedy booker/producer. If you see me tweeting stop me brett@yahoonews.com",
+			"url": "https://t.co/SgwR6EbK2Z",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SgwR6EbK2Z",
+							"expanded_url": "https://linktr.ee/thenewflesh",
+							"display_url": "linktr.ee/thenewflesh",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5970,
+			"friends_count": 983,
+			"listed_count": 128,
+			"created_at": "Wed Dec 31 18:04:02 +0000 2008",
+			"favourites_count": 317111,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 149938,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1454960730273374209/0rjyc_Oh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1454960730273374209/0rjyc_Oh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18511844/1622943532",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 173,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/qROuktiV4k"
+	},
+	{
 		"created_at": "Fri Nov 05 02:36:40 +0000 2021",
 		"id": 1456450362836201500,
 		"id_str": "1456450362836201486",
